8ad1d027696a83e60fc998f40c95e33beda695ab
Alex Krautmann <alex.krautmann@gmail.com>
2019-04-04 21:52:23 -0500

    react-native-web css can now ssr

--- packages/apps/web-app/pages/_document.tsx
+++ packages/apps/web-app/pages/_document.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import Document from 'next/document'
 import {ServerStyleSheet} from 'styled-components'
+import ReactDOMServer from 'react-dom/server';
+import { AppRegistry } from 'react-native-web';
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
@@ -10,13 +12,20 @@ export default class MyDocument extends Document {
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
-                    enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+                    enhanceApp: App => props => {
+                        // TODO: this could possible pass the app returned from AppRegistry.registerComponent as the App in sheet.collectStyles
+                        AppRegistry.registerComponent('App', () => App);
+                        return sheet.collectStyles(<App {...props} />)
+                    }
                 })
 
             const initialProps = await Document.getInitialProps(ctx)
+
+            const { getStyleElement } = AppRegistry.getApplication('App', { initialProps });
+
             return {
                 ...initialProps,
-                styles: <>{initialProps.styles}{sheet.getStyleElement()}</>
+                styles: <>{initialProps.styles}{sheet.getStyleElement()}{getStyleElement()}</>
             }
         } finally {
             sheet.seal()
